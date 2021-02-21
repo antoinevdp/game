838e69ef64b036781c6e9533073788b7c5cc7b15
lukstrikers <74832649+lukstrikers@users.noreply.github.com>
2021-02-21 12:27:29 +0100

    Inventory sys start

---
+++ Game Lucas Antoine/Assets/Scriptable Objects/Inventory/Scripts/InventoryObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : ScriptableObject
+{
+    //Store InventorySlot objects
+    public List<InventorySlot> Container = new List<InventorySlot>();
+}
+
+[System.Serializable]
+public class InventorySlot
+{
+    //Store The actual item aand amount on a slot
+    public ItemObject item;
+    public int amount;
+
+    public InventorySlot(ItemObject _item, int _amount)
+    {
+        item = _item;
+        amount = _amount;
+    }
+
+    public void Addamount(int value)
+    {
+        amount += value;
+    }
+}
---
+++ Game Lucas Antoine/Assets/Scriptable Objects/Items/Scripts/EquipmentObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment Object", menuName = "Inventory System/Items/Equipment")]
+public class EquipmentObject : ItemObject
+{
+    public float speedMovementMultiplier;
+    public float attackMultiplier;
+    public float protectionValue;
+    public float radiationProtectionValue;
+    public void Awake()
+    {
+        type = ItemType.Equipment;
+    }
+}
